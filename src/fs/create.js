51ec1ff709853dae89e1cc037683be647c03409b
Pavel <pavel.priladyshev@gmail.com>
2023-01-31 14:43:23 +0300

    feat: add create file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,13 @@
+import { writeFile } from 'fs';
+
 const create = async () => {
-    // Write your code here 
+
+    writeFile('src/fs/files/fresh.txt', 'I am fresh and young', (error) => {
+        if (error) {
+            console.log("FS operation failed", error);
+        }
+    })
+
 };
 
 await create();
