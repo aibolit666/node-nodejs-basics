aed1f097d4381b306a9e44cdda96e2a819d96795
Pavel <pavel.priladyshev@gmail.com>
2023-01-31 15:11:03 +0300

    fix: error print

--- src/fs/create.js
+++ src/fs/create.js
@@ -4,7 +4,7 @@ const create = async () => {
 
     writeFile('src/fs/files/fresh.txt', 'I am fresh and young', (error) => {
         if (error) {
-            console.log("FS operation failed", error);
+            console.log("FS operation failed");
         }
     })
 
